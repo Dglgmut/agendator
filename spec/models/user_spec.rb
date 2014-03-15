2fb6daeeb7cbe563439e59def1fea263520bf2e9
dglgmut <florez.renan@gmail.com>
2014-03-15 08:16:33 -0300

    creating specs for users and validation for user's name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "name" do
+    it { should validate_presence_of :name }
+  end
+
+  describe "email" do
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of(:email) }
+  end
+
+  describe "password" do
+    it { should validate_presence_of :password }
+    it { should validate_confirmation_of :password}
+  end
 end
