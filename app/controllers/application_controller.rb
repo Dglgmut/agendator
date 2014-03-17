0fd209801bfc5a3ba77cddf9888260b88439884c
dglgmut <florez.renan@gmail.com>
2014-03-17 02:21:08 -0300

    refactoring controller for a more meaningfull name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :authenticate_user!
-  before_filter :auth_user
+  before_filter :redirect_to_sign_up_on_first_access
 
   protected
-    def auth_user
+    def redirect_to_sign_up_on_first_access
       redirect_to new_user_registration_url if first_time
     end
 
