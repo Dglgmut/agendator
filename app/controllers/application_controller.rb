9b41c7d1d8fedb8db56d234e7e8ea092d2dde0bb
dglgmut <florez.renan@gmail.com>
2014-03-17 02:20:04 -0300

    redirecting user to signup if its his first time

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :authenticate_user!
+  before_filter :auth_user
+
+  protected
+    def auth_user
+      redirect_to new_user_registration_url if first_time
+    end
+
+    def first_time
+      unless cookies[:first_time]
+        return cookies[:first_time] = true
+      end
+    end
 end
